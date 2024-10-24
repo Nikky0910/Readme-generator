994d088c2f37f21009442d8e21c14fabec4b688f
Marianna <mariannaniko1092@gmail.com>
2024-10-23 21:25:38 -0400

    Add function to initialize the app

--- index.js
+++ index.js
@@ -56,7 +56,12 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions).then((data)=> {
+            writeToFile("./output/README.md", data)
+        })
+}
 
 // Function call to initialize app
 init();
